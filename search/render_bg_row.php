d4b65d3618c029aac7508c33016921d5e8906952
Dan Green <dgreen314@gmail.com>
2017-05-03 16:08:21 -0600

    Added some stuff to the rows.

--- search/render_bg_row.php
+++ search/render_bg_row.php
@@ -1,6 +1,6 @@
 <?php
 function render_bg_row($data) {
-	print('<tr>');
+	print('<tr class="bg-row">');
 	//print('<td>');print_r($data);print('</td>');
 	$name=$data['primaryName'];
 	$thumbnail = $data['thumbnail'];
@@ -9,11 +9,18 @@ function render_bg_row($data) {
 	$playerAge = $data['playerAge'];
 	$playingTime = $data['playingTime'];
 	$desc = $data['description'];
-	$weight = $data['avgWeight'];
-	$rating = $data['avgRating'];
+	if($data['numWeighted'] == 0) { $weight = '-'; }
+	else { $weight = round($data['avgWeight'], 2); }
+	if($data['numRated'] == 0) { $rating = '-'; }
+	else { $rating = round($data['avgRating'], 2); }
 	print("<td><img src='$thumbnail'/></td>"
 		  .'<td>'
-		  ."<h2>$name</h2>"
+		  .'<div>'
+		  ."<h2 class='primary-name'>$name</h2>"
+		  .'<div class="ratings">'
+		  ."<p title='Rating'><i class='fa fa-star' aria-hidden='true'></i>&nbsp;$rating / 10</p>"
+		  ."<p title='Complexity'><i class='fa fa-puzzle-piece' aria-hidden='true'></i>&nbsp;$weight / 5</p></div>"
+		  .'</div>'
 		  .'</td></tr>');
 }
 ?>

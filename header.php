1d531305abbba53c2f7c79552db760a47a0c87c1
Michael Martin <ss013r@unm.edu>
2017-05-03 16:03:26 -0600

    added some css in

--- header.php
+++ header.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title><?php echo $title; ?></title>
+    
 
     <!-- Bootstrap -->
     <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
@@ -15,6 +17,9 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    <link rel="stylesheet" type="text/css" href="/static/css/main.css">
+
   </head>
   <body>
 
